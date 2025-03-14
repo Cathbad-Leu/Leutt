a18af60dd6c345d3d51e4e25b13bc628bf7b6e2d
Federico B. <fb.internet@outlook.com>
2025-03-14 23:33:11 +0100

    news

--- leu.bat
+++ leu.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-start "" .\bin\mintty.exe -c mintty.con -i .\leu.ico -e .\tt++.exe -t Lumen_et_Umbra -r .\main.tin
+start "" .\bin\mintty.exe -c mintty.con -i .\leu.ico -e .\bin\tt++.exe -t Lumen_et_Umbra -r .\main.tin
